8a1790d8a307e38d92d94cc6ddc439e7ee7758e5
lillianferreira <lillian_af@hotmail.com>
2023-01-31 21:02:53 -0300

    Criando meu portfólio

---
+++ script/escrever.js
@@ -0,0 +1,18 @@
+const meuNome = document.querySelector('.meu-nome');
+const fName = meuNome.innerHTML;
+const botaoVerMais = document.querySelector('button');
+const habilidades = document.querySelectorAll('.escondido');
+const node = document.querySelector('#fundo')
+meuNome.innerHTML='';
+
+function typeWriter (nome) {
+    setTimeout( () => {
+    const meuNomeF = nome.split('');
+    meuNome.innerHTML='';
+    meuNomeF.forEach( (letra, index) => {
+        setTimeout(function (){
+            meuNome.innerHTML += letra;
+        }, 150 * index)
+    })}, 1500)
+
+}
