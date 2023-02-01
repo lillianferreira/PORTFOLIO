6319ca02220662a3d8849ac33c4a418d72e6a68d
lillianferreira <lillian_af@hotmail.com>
2023-02-01 08:02:16 -0300

    Atualizando portfólio

--- script/escrever.js
+++ script/escrever.js
@@ -5,6 +5,24 @@ const habilidades = document.querySelectorAll('.escondido');
 const node = document.querySelector('#fundo')
 meuNome.innerHTML='';
 
+function cliqueVer (botaoVerMais) {
+    habilidades.forEach( (elemento) => {
+        if (elemento.classList.contains('escondido')) {
+            elemento.classList.remove('escondido');
+            elemento.classList.add('visivel');
+            botaoVerMais.innerHTML = 'Ver menos';
+        } else {
+            elemento.classList.add('escondido');
+            elemento.classList.remove('visivel');
+            botaoVerMais.innerHTML = 'Ver mais';
+        }
+    } )
+}
+
+function maisHabilidades (elemento) {
+    elemento.addEventListener('click', cliqueVer)
+}
+
 function typeWriter (nome) {
     setTimeout( () => {
     const meuNomeF = nome.split('');
@@ -16,3 +34,6 @@ function typeWriter (nome) {
     })}, 1500)
 
 }
+
+/* typeWriter(fName); */
+maisHabilidades(botaoVerMais);
